f8b984d17563a4701949fa0a3f55b02ef2d0368e
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2021-08-24 19:03:17 +0200

    update decs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pytest-fixture-typecheck",
-    description="A pytest plugin to ensure test fixtures have the right type.",
+    description="A pytest plugin to assert type annotations at runtime.",
     long_description=open("README.rst").read(),
     author="Markus Unterwaditzer",
     version="0.1.0",
