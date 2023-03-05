67a0919eea4394877dee60375f80622837ab7dc2
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2023-03-05 19:37:11 +0100

    version 0.1.1

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     description="A pytest plugin to assert type annotations at runtime.",
     long_description=open("README.rst").read(),
     author="Markus Unterwaditzer",
-    version="0.1.0",
+    version="0.1.1",
     license="Public Domain",
     author_email="markus@unterwaditzer.net",
     url="https://github.com/untitaker/pytest-fixture-typecheck",
-    py_modules=["pytest_sentry"],
+    py_modules=["pytest_fixture_typecheck"],
     entry_points={"pytest11": ["fixture_typecheck = pytest_fixture_typecheck"]},
     install_requires=["pytest", "typeguard"],
 )
